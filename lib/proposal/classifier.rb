49414ebf37ec895075f505da000d2829326dd1df
chaydson <chaydson14@gmail.com>
2023-09-26 04:03:16 -0300

    first commit

---
+++ lib/proposal/classifier.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require 'onnxruntime'
+require 'ruby-spacy'
+require 'pycall/import'
+include PyCall::Import
+
+require_relative "classifier/version"
+
+module Proposal
+  module Classifier
+    class Error < StandardError; end
+      def predict_onx(input_text)
+        nlp = Spacy::Language.new("pt_core_news_lg")
+        doc = nlp.read(input_text)
+        vector = doc.vector
+        scaled_array = (vector - vector.min) / (vector.max - vector.min) * (1 - 0) + 0
+        model = OnnxRuntime::InferenceSession.new("lib/proposal/exported_model.onnx")
+        label_name = model.outputs()[0][:name]
+        vector_fim_reshaped = scaled_array.reshape(1, -1)
+        pred_onx = model.run([label_name], {X: vector_fim_reshaped})[0]
+        return pred_onx
+      end
+  end
+end
