b1906ccb49cd20c324b4cae89f23020cc78b6408
chaydson <chaydson14@gmail.com>
2023-11-13 03:58:59 -0300

    convert and copy reponse string

--- lib/proposal/classifier.rb
+++ lib/proposal/classifier.rb
@@ -5,23 +5,23 @@ require "http"
 module Proposal
   module Classifier
     class Model
-      @@base_url = "http://0.0.0.0"  # Default base URL
+      @base_url = "http://0.0.0.0"  # Default base URL
 
       def self.base_url
-        @@base_url
+        @base_url
       end
 
       def self.base_url=(new_url)
-        @@base_url = new_url
+        @base_url = new_url
       end
 
       def self.init()
-        response_get = HTTP.get(@@base_url).body.to_s
+        response_get = HTTP.get(@base_url).body.to_s.dub
         return response_get
       end
 
       def self.predict(input_text)
-        response_post = HTTP.post("#{@@base_url}/predict", :json => { :text => input_text }).body.to_s
+        response_post = HTTP.post("#{@base_url}/predict", :json => { :text => input_text }).body.to_s.dub
         return response_post
       end
     end
