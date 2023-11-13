896c3c28a2d99c02c23e48e497e940298c041d58
chaydson <chaydson14@gmail.com>
2023-11-13 04:09:12 -0300

    clocando as string

--- lib/proposal/classifier.rb
+++ lib/proposal/classifier.rb
@@ -8,21 +8,21 @@ module Proposal
       @base_url = "http://0.0.0.0"  # Default base URL
 
       def self.base_url
-        @base_url.dup
+        @base_url.clone
       end
 
       def self.base_url=(new_url)
-        @base_url = new_url.dup
+        @base_url = new_url.clone
       end
 
       def self.init()
-        response_get = HTTP.get(@base_url.dup).body.to_s.dup
-        return response_get.dup
+        response_get = HTTP.get(@base_url.clone).body.to_s.clone
+        return response_get.clone
       end
 
       def self.predict(input_text)
-        response_post = HTTP.post("#{@base_url.dup}/predict", :json => { :text => input_text }).body.to_s.dup
-        return response_post.dup
+        response_post = HTTP.post("#{@base_url.clone}/predict", :json => { :text => input_text }).body.to_s.clone
+        return response_post.clone
       end
     end
   end
