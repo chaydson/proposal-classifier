f3e7acba950918d00a4327651b9cbae2a99f4b8a
chaydson <chaydson14@gmail.com>
2023-11-13 03:38:22 -0300

    adding a copy of the base_url string to resolve freeze string error

--- lib/proposal/classifier.rb
+++ lib/proposal/classifier.rb
@@ -6,21 +6,21 @@ module Proposal
       @@base_url = "http://0.0.0.0"  # Default base URL
 
       def self.base_url
-        @@base_url
+        @@base_url.dup
       end
 
       def self.base_url=(new_url)
-        @@base_url = new_url
+        @@base_url = new_url.dup
       end
 
       def self.init()
-        response_get = HTTP.get("#{@@base_url}")
+        response_get = HTTP.get(@@base_url.dup)
         formatted_get_response = response_get.parse
         return formatted_get_response
       end
 
       def self.predict(input_text)
-        response_post = HTTP.post("#{@@base_url}/predict", :json => { :text => input_text })
+        response_post = HTTP.post("#{@@base_url.dup}/predict", :json => { :text => input_text })
         formatted_post_response = response_post.parse
         return formatted_post_response
       end
